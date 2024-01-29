16159fd8705e97f0904891dfc40987a7a0e6a545
LeeJoEun-01 <joeun1005@naver.com>
2024-01-29 16:04:03 +0900

    [Feat] 스크롤 이벤트 발생 시 색상 변화 함수 적용

--- Scene/Scene/Test/Glass_TestView.swift
+++ Scene/Scene/Test/Glass_TestView.swift
@@ -19,32 +19,41 @@ struct TestView: View {
 struct Test : View {
 
     @State var models = [
-        Model(id: 0, name: "Earth", modelName: "head05.scn", details: "test")
+        Model(id: 0, name: "1", modelName: "Concrete-Smooth.usdz", details: "test"),
+        Model(id: 1, name: "2", modelName: "Concrete-smooth-legacy.usdz", details: "test"),
+        Model(id: 2, name: "3", modelName: "Asphalt_withCrack-Only-LightOff.usdz", details: "test"),
+        Model(id: 3, name: "4", modelName: "Asphalt_withCrack-All-SphereOnly.usdz", details: "test"),
+        Model(id: 4, name: "5", modelName: "Asphalt_withCrack-All-SphereOnly_light.usdz", details: "test"),
+        Model(id: 5, name: "6", modelName: "Asphalt_withCrack-All-SphereOnly-Plane.usdz", details: "test"),
+        Model(id: 6, name: "7", modelName: "Asphalt_withCrack-All-SphereOnly-Plane_light.usdz", details: "test"),
+        Model(id: 7, name: "8", modelName: "Asphalt_withCrack-All-SphereOnly-Plane_4.usdz", details: "test")
     ]
 
-    @State var index = 0
+    @State var index = 7
 
     var body: some View{
         let scene = SCNScene(named: models[index].modelName)
         scene?.background.contents = UIColor.black
 
+        // 조명을 생성하고 설정
+        let light = SCNLight()
+        light.type = .ambient // 또는 .ambient, .directional, .spot 등 다른 타입을 선택할 수 있습니다.
+        light.color = UIColor.systemPink // 원하는 색상을 설정합니다.
+//
+//        let lightNode = SCNNode()
+//        lightNode.light = light
+//        lightNode.position = SCNVector3(x: 0, y: 50, z: -10) // 원하는 위치를 설정합니다.
+//        scene?.rootNode.addChildNode(lightNode)
+
         return ScrollView {
             VStack{
-
-                // Going to use SceneKit Scene View....
-
-                // default is first object ie: Earth...
-
-                // Scene View Has a default Camera View...  
-                // if you nedd custom means add there...
                 SceneView(scene: scene, options: [.autoenablesDefaultLighting,.allowsCameraControl])
-                // for user action...
-                // setting custom frame...
                     .frame(width: UIScreen.main.bounds.width , height: UIScreen.main.bounds.height)
 
             }
         }
     }
+
 }
 
 // Sample Data...
--- Scene/Scene/Test/TestModelView1.swift
+++ Scene/Scene/Test/TestModelView1.swift
@@ -4,35 +4,38 @@ import SceneKit
 
 struct TestModelView1: View {
     @State var rotationDuration: TimeInterval = 2.0
-
+    
     var body: some View {
         VStack {
-            if rotationDuration > 6.0 {
-                AnimationView()
-                    .transition(.opacity)
-            } else {
-                TestModel(rotationDuration: $rotationDuration)
-                    .transition(.opacity)
-                    .edgesIgnoringSafeArea(.all)
-                    .background(Color.black)
-            }
+            TestModel(rotationDuration: $rotationDuration)
+                .transition(.opacity)
+                .edgesIgnoringSafeArea(.all)
+                .background(Color.black)
         }
     }
 }
 
 struct TestModel: View {
-
+    
     @State var index = 0
     @State var isRolling = false
     @State var lastDragAmount: CGFloat = 0
     @Binding var rotationDuration: TimeInterval
     @State var rotationPi: Double = .pi
+    @State private var timer: Timer? = nil
+
+    @State var velocity: CGFloat = 30
 
     @State var glassHead: SCNScene? = SCNScene(named: "head05.scn") // Add this line
-
+    
+    @State var red: CGFloat = 0.5
+    @State var green: CGFloat = 0.5
+    @State var blue: CGFloat = 0.5
+    let alpha: CGFloat = 1.0
+    
     var body: some View {
-        let crackScene = SCNScene(named: "Asphalt_withCrack.scn")
-
+        let crackScene = SCNScene(named: "Asphalt_withCrack-All-SphereOnly-Plane.usdz")
+        
         let backgroundImage = UIImage(named: "asphalt")
         glassHead?.background.contents = backgroundImage
         crackScene?.background.contents = UIColor.black
@@ -47,30 +50,82 @@ struct TestModel: View {
                     .gesture(
                         DragGesture()
                             .onChanged { change in
+                                self.timer?.invalidate()
+                                self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                                    print("no event")
+                                    changeAnimation(0.5, 0.5, 0.5)
+                                }
+                                
                                 if change.translation.height > 0 {
+                                    print("위")
+                                    changeAnimation(0.5, 0.5, 1.0)
                                     withAnimation {
                                         self.rotationDuration -= 0.6
                                     }
-                                    let rotationAction = SCNAction.rotate(by: .pi*2, around: SCNVector3(1, 1, 0), duration: self.rotationDuration)
+                                    let rotationAction = SCNAction.rotate(by: .pi*2, around: SCNVector3(1, 0, 0), duration: self.rotationDuration)
+                                    // 속도 갱신
                                     glassHead?.rootNode.runAction(rotationAction)
-                                    print("위로 올라가는: \(rotationDuration)")
+                                    print("위로 올라가는: \(velocity)")
                                 } else if change.translation.height < 0 {
+                                    changeAnimation(1.0, 0.5, 0.5)
                                     withAnimation {
                                         self.rotationDuration += 0.6
                                     }
-                                    let rotationAction = SCNAction.rotate(by: .pi*2, around: SCNVector3(1, 1, 0), duration: self.rotationDuration)
+                                    let rotationAction = SCNAction.rotate(by: .pi*2, around: SCNVector3(1, 0, 0), duration: self.rotationDuration)
                                     glassHead?.rootNode.runAction(rotationAction)
-                                    print("아래로 내려가는: \(rotationDuration)")
+                                    print("아래로 내려가는: \(velocity)")
+                                    print("아래")
+                                } else if change.translation.width > 0 {
+                                    print("오른쪽")
+                                } else if change.translation.width < 0 {
+                                    print("왼쪽")
                                 }
                             }
+                            .onEnded { _ in
+                                // 사용자가 드래그를 끝내면 타이머를 초기화
+                                self.timer?.invalidate()
+                                self.timer = nil
+                            }
                     )
-
+                
                 // Front
                 SceneView(scene: glassHead, options: [.autoenablesDefaultLighting,.allowsCameraControl])
                     .frame(width: UIScreen.main.bounds.width/4+80 , height: UIScreen.main.bounds.height/4, alignment: .center)
                     .background(Color.clear)
                     .clipShape(Circle())
                     .position(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2)
+            }
+        }
+    }
+
+    // 색상 변경 함수
+    func changeColor(_ goalRed: CGFloat, _ goalGreen: CGFloat, _ goalBlue: CGFloat) -> UIColor {
+        print("=== color change func 🎨 ===")
+        let newRed = self.red + (goalRed - self.red)/velocity
+        self.red = newRed
+        let newGreen = self.green + (goalGreen - self.green)/velocity
+        self.green = newGreen
+        let newBlue = self.blue + (goalBlue - self.blue)/velocity
+        self.blue = newBlue
+
+        // print("🌀🌀newBlue: \(self.blue)")
+        print("🌀🌀🌀newBlue: \(self.blue + (goalBlue - self.blue)/velocity)")
+
+        let newColor = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        return newColor
+    }
+
+    func changeAnimation(_ goalRed: CGFloat, _ goalGreen: CGFloat, _ goalBlue: CGFloat) {
+        print("=== changeAnimation func 📽️ ===")
+        glassHead?.rootNode.enumerateChildNodes { node, _ in
+            node.geometry?.materials.forEach { material in
+                let newColor = changeColor(goalRed, goalGreen, goalBlue)
+
+                // SCNTransaction을 사용하여 애니메이션 적용
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5 // 애니메이션 지속 시간 설정
+                material.diffuse.contents = newColor
+                SCNTransaction.commit()
             }
         }
     }
